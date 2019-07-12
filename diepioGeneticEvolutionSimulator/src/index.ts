d3e3ae5a523217d4803953144067d922787a2d9d
JaPNaA <leonehuang100@gmail.com>
2019-07-11 20:00:24 -0400

    remove Evolver

--- diepioGeneticEvolutionSimulator/src/index.ts
+++ diepioGeneticEvolutionSimulator/src/index.ts
@@ -1,12 +1,9 @@
 import Game from "./game/Game";
-import Evolver from "./evolver/Evolver";
 
 const game = new Game();
-const evolver = new Evolver(game);
 game.appendTo(document.body);
 
 console.log(game);
-console.log(evolver);
 
 // @ts-ignore
 window.game = game;
