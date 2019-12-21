fee2251f9b1a6e743e80e0ff065d92c066d50045
JaPNaA <leonehuang100@gmail.com>
2019-12-20 23:55:34 -0500

    update views

--- countdown/src/view/views/countdownView.ts
+++ countdown/src/view/views/countdownView.ts
@@ -1,13 +1,21 @@
 import View from "../view.js";
 import { getElmById, round } from "../../utils.js";
-import { getClosestDateDifferenceMilliseconds, dateDiffToString, getClosestDateDifference, millisecondsThisYear } from "../../date.js";
+import { getClosestDateDifferenceMilliseconds, getClosestDateDifference, millisecondsThisYear } from "../../date.js";
 
-const birthday = new Date(2020, 0, 1);
+const birthday = new Date(2003, 10, 19);
 console.log(birthday);
 
 
 class _CountdownView extends View {
-    private timerElm = getElmById("timer");
+    private yearsElm = getElmById("countdownYears");
+    private monthsElm = getElmById("countdownMonths");
+    private daysElm = getElmById("countdownDays");
+    private hoursElm = getElmById("countdownHours");
+    private minutesElm = getElmById("countdownMinutes");
+    private secondsElm = getElmById("countdownSeconds");
+    private millisecondsElm = getElmById("countdownMilliseconds");
+    private totalmillisecondsElm = getElmById("countdownTotalMilliseconds");
+    private totalYearsElm = getElmById("countdownTotalYears");
 
     constructor() {
         super(getElmById("countdown"));
@@ -21,13 +29,21 @@ class _CountdownView extends View {
     private requestAnimationFrameCallback() {
         const now = new Date();
 
+        const diff = getClosestDateDifference(now, birthday);
         const msDiff = getClosestDateDifferenceMilliseconds(now, birthday);
 
-        this.timerElm.innerText =
-            dateDiffToString(
-                getClosestDateDifference(now, birthday)
-            ) +
-            " (" + msDiff + " milliseconds, " + round(msDiff / millisecondsThisYear(), 0.0001) + " years)";
+        this.yearsElm.innerText = diff.years.toString();
+        this.monthsElm.innerText = diff.months.toString();
+        this.daysElm.innerText = diff.days.toString();
+        this.hoursElm.innerText = diff.hours.toString();
+        this.minutesElm.innerText = diff.minutes.toString();
+        this.secondsElm.innerText = diff.seconds.toString();
+        this.millisecondsElm.innerText = diff.milliseconds.toString();
+
+        this.totalmillisecondsElm.innerText = msDiff.toString();
+        this.totalYearsElm.innerText = (msDiff / millisecondsThisYear()).toString();
+
+        console.log(diff.negative);
 
         requestAnimationFrame(() => this.requestAnimationFrameCallback());
     }
